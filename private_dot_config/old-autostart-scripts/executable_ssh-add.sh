62c7639c5c2d12a106af7c4df8d2bc3eb13e15df
Jason Crome <jason.crome@foundant.com>
2024-10-07 14:08:08 -0400

    Add Foundant config

--- private_dot_config/old-autostart-scripts/executable_ssh-add.sh
+++ private_dot_config/old-autostart-scripts/executable_ssh-add.sh
@@ -2,5 +2,5 @@
 
 export SSH_ASKPASS=/usr/bin/ksshaskpass
 
-ssh-add $HOME/.ssh/id_rsa $HOME/.ssh/rtadmin_rsa $HOME/.ssh/softserve_rsa
+ssh-add $HOME/.ssh/foundant_rsa $HOME/.ssh/rtadmin_rsa $HOME/.ssh/softserve_rsa
 
