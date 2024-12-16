5e56feb971ac4cefa3f34955fc21e03d3d7c481a
Jason A. Crome <jason@crome-plated.com>
2024-12-16 16:32:16 -0500

    Remove unused paths from shell config

--- dot_shell/executable_path.sh
+++ dot_shell/executable_path.sh
@@ -3,6 +3,6 @@ export PATH="$HOME/bin:$HOME/.local/bin:$HOME/.tmuxifier/bin:/usr/local/bin:/usr
 osname=$(uname)
 if [ "$osname" = "Darwin" ]; then
     export PATH="/opt/local/bin:/opt/local/sbin:/opt/local/lib/mariadb-10.6/bin/:/Applications/Postgres.app/Contents/Versions/15/bin:$PATH"
-else
-    export PATH="$HOME/Applications/datagrip/bin:$HOME/Applications/pycharm/bin:$PATH"
+#else
+#    export PATH="$HOME/Applications/datagrip/bin:$HOME/Applications/pycharm/bin:$PATH"
 fi
