edb5cf899a25b6a0b42c702fdc129b446c7cebe8
Jason Crome <jason.crome@foundant.com>
2025-01-16 09:32:24 -0500

    Udpate fzf settings

--- dot_shell/fzf.zsh
+++ dot_shell/fzf.zsh
@@ -5,6 +5,8 @@ if [ "$osname" != "OpenBSD" ]; then
     export FZF_CTRL_R_COMMAND="$FZF_DEFAULT_COMMAND"
 fi
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="fd -t d . $HOME"
+#export FZF_ALT_C_COMMAND="fd -t d . $HOME"
+export FZF_ALT_C_COMMAND=""
 
+source <(fzf --zsh)
 source ~/.shell/fzf-key-bindings.zsh
