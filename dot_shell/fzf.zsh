ca9357f52c74ae4a076732b184c13b26645d40eb
Jason A. Crome <jason@crome-plated.com>
2025-06-02 09:43:01 -0400

    Numerous netbsd updates

--- dot_shell/fzf.zsh
+++ dot_shell/fzf.zsh
@@ -2,6 +2,6 @@ osname=$(uname)
 
 #export FZF_DEFAULT_COMMAND="fd . $HOME"
 #export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-#export FZF_ALT_C_COMMAND="fd -t d . $HOME"
+export FZF_ALT_C_COMMAND=""
 
 source ~/.shell/fzf-key-bindings.zsh
